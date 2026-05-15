41ec80ce6f177173ab6b88641670db86adea6de6
Shivam Kumar Kashyap <122219187+SHIVAMKASHYAP5073@users.noreply.github.com>
2026-05-15 12:57:33 +0530

    Merging two list

--- Review/Merge.java
+++ Review/Merge.java
@@ -1,1 +1,28 @@
+class Quick{
+    public static void main(String[] args) {
+        int nums[] = {1, 7, 3, 6, 5, 6};
 
+        int totalSum = 0;
+
+        
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+          
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                System.out.println("Pivot Index = " + i);
+                return;
+            }
+
+            leftSum += nums[i];
+        }
+
+        
+        System.out.println("Pivot Index = -1");
+    }
+}
